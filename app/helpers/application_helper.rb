b43805c11868f44a08491a75d446e9ec30424e96
juxsalley <juxsalley@gmail.com>
2021-03-26 13:02:29 +0000

    add destop, mobile and tablet reponsiveseness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,12 +17,10 @@ module ApplicationHelper
         end
     end
     def render_navbar
-
         if current_user
             render partial: 'shared/navbar'
         else 
-            render partial: 'shared/new'
-            
+            render partial: 'shared/new'    
         end
     end
 
@@ -31,8 +29,7 @@ module ApplicationHelper
             "<span class='flex h-4 w-4 absolute -top-1 -right-1'>
                 <span class='animate-ping absolute inline-flex h-full w-full rounded-full bg-green-200 opacity-75'></span>
                 <span class=' inline-flex absolute rounded-full h-4 w-4 bg-green-100'></span>
-            </span>".html_safe
-                
+            </span>".html_safe              
         end
     end
 
@@ -50,13 +47,12 @@ module ApplicationHelper
         else
             'icon.png'
         end
-
     end
     def set_icon(group)
         if group.icon.attached? 
-            image_tag(group.icon, class: 'w-1/5') 
+            image_tag(group.icon, class: 'w-1/5 md:w-2/6') 
         else
-            image_tag('icon.png', class: 'w-1/5') 
+            image_tag('icon.png', class: 'w-1/5 md:w-2/6') 
             
         end
     end
