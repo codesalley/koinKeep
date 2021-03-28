d8201e8e53bec4af86af749d80ca1c5dd2a1c8c2
juxsalley <juxsalley@gmail.com>
2021-03-28 22:38:28 +0000

    add falsy testing testing to transactions

--- spec/features/transactions_spec.rb
+++ spec/features/transactions_spec.rb
@@ -16,5 +16,16 @@ RSpec.feature 'Transactions', type: :feature do
         expect(page).to have_http_status(200)
       end
     end
+
+    scenario 'logged user can create a group transaction' do
+      visit '/transactions/new'
+      within('form') do
+        fill_in 'transaction[name]', with: nil
+        fill_in 'transaction[amount]', with: nil
+        find_button('Add Transaction').click
+      end
+      expect(page).to have_http_status(200)
+      expect(page).to have_text(/Error adding transaction/)
+    end
   end
 end
